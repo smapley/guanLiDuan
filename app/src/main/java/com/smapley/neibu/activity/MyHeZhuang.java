889a9c64ccfc614410c60b3736e937bed0dc9407
smapley <912122724@qq.com>
2016-07-12 01:16:45 +0800

    修改合庄

--- app/src/main/java/com/smapley/neibu/activity/MyHeZhuang.java
+++ app/src/main/java/com/smapley/neibu/activity/MyHeZhuang.java
@@ -34,10 +34,26 @@ public class MyHeZhuang extends AppCompatActivity {
     @ViewInject(R.id.title_item3)
     private TextView item3;
 
-    @ViewInject(R.id.zhuang_item1)
-    private EditText editText1;
-    @ViewInject(R.id.zhuang_item2)
-    private EditText editText2;
+    @ViewInject(R.id.zhuang_item11)
+    private EditText editText11;
+    @ViewInject(R.id.zhuang_item12)
+    private EditText editText12;
+    @ViewInject(R.id.zhuang_item13)
+    private EditText editText13;
+    @ViewInject(R.id.zhuang_item14)
+    private EditText editText14;
+    @ViewInject(R.id.zhuang_item15)
+    private EditText editText15;
+    @ViewInject(R.id.zhuang_item16)
+    private EditText editText16;
+    @ViewInject(R.id.zhuang_item21)
+    private EditText editText21;
+    @ViewInject(R.id.zhuang_item22)
+    private EditText editText22;
+    @ViewInject(R.id.zhuang_item23)
+    private EditText editText23;
+    @ViewInject(R.id.zhuang_item24)
+    private EditText editText24;
     @ViewInject(R.id.zhuang_item3)
     private EditText editText3;
     @ViewInject(R.id.zhuang_item4)
@@ -48,13 +64,24 @@ public class MyHeZhuang extends AppCompatActivity {
     private EditText editText6;
 
 
-    private GetGhzService getGhzService=new GetGhzService() {
+    private GetGhzService getGhzService = new GetGhzService() {
         @Override
         public void Succ(String data) {
-            Map<String,String> result= JSON.parseObject(data,new TypeReference<Map<String, String>>(){});
-            if(result!=null){
-                editText1.setText(result.get("erdj"));
-                editText2.setText(result.get("sandj"));
+            Map<String, String> result = JSON.parseObject(data, new TypeReference<Map<String, String>>() {
+            });
+            if (result != null) {
+                editText11.setText(result.get("erdj1"));
+                editText12.setText(result.get("erdj2"));
+                editText13.setText(result.get("erdj3"));
+                editText14.setText(result.get("erdj4"));
+                editText15.setText(result.get("erdj5"));
+                editText16.setText(result.get("erdj6"));
+
+                editText21.setText(result.get("sandj1"));
+                editText22.setText(result.get("sandj2"));
+                editText23.setText(result.get("sandj3"));
+                editText24.setText(result.get("sandj4"));
+
                 editText3.setText(result.get("sidj"));
                 editText4.setText(result.get("erxj"));
                 editText5.setText(result.get("sanxj"));
@@ -63,11 +90,12 @@ public class MyHeZhuang extends AppCompatActivity {
         }
     };
 
-    private UpdateGhzService updateGhzService=new UpdateGhzService() {
+    private UpdateGhzService updateGhzService = new UpdateGhzService() {
         @Override
         public void Succ(String data) {
-            String result= JSON.parseObject(data,new TypeReference<String>(){});
-            Toast.makeText(MyHeZhuang.this,result,Toast.LENGTH_SHORT).show();
+            String result = JSON.parseObject(data, new TypeReference<String>() {
+            });
+            Toast.makeText(MyHeZhuang.this, result, Toast.LENGTH_SHORT).show();
         }
     };
 
@@ -88,11 +116,11 @@ public class MyHeZhuang extends AppCompatActivity {
 
     private void initView() {
         item1.setText("账单");
-        item2.setText("合庄");
+        item2.setText("拦码");
         item3.setText("完成");
     }
 
-    @Event({R.id.title_item1,R.id.title_item3})
+    @Event({R.id.title_item1, R.id.title_item3})
     private void onClick(View view) {
         switch (view.getId()) {
             case R.id.title_item1:
@@ -106,23 +134,41 @@ public class MyHeZhuang extends AppCompatActivity {
 
     private void update() {
         try {
-            String erdj = null;
-            String sandj = null;
+            String erdj1 = null;
+            String erdj2 = null;
+            String erdj3 = null;
+            String erdj4 = null;
+            String erdj5 = null;
+            String erdj6 = null;
+
+            String sandj1 = null;
+            String sandj2 = null;
+            String sandj3 = null;
+            String sandj4 = null;
+
             String sidj = null;
             String erxj = null;
             String sanxj = null;
             String sixj = null;
-            erdj = editText1.getText().toString();
-            sandj = editText2.getText().toString();
+            erdj1 = editText11.getText().toString() ;
+            erdj2 = editText12.getText().toString() ;
+            erdj3 = editText13.getText().toString();
+            erdj4 = editText14.getText().toString();
+            erdj5 = editText15.getText().toString();
+            erdj6 = editText16.getText().toString() ;
+            sandj1 = editText21.getText().toString();
+            sandj2 = editText22.getText().toString();
+            sandj3 = editText23.getText().toString() ;
+            sandj4 = editText24.getText().toString() ;
             sidj = editText3.getText().toString();
             erxj = editText4.getText().toString();
             sanxj = editText5.getText().toString();
             sixj = editText6.getText().toString();
 
-            updateGhzService.load(new UpdateGhzParams(MyData.UserName, erdj,sandj,sidj,erxj,sanxj,sixj));
-        }catch (Exception exception){
+            updateGhzService.load(new UpdateGhzParams(MyData.UserName, erdj1, erdj2, erdj3, erdj4, erdj5, erdj6, sandj1, sandj2, sandj3, sandj4, sidj, erxj, sanxj, sixj));
+        } catch (Exception exception) {
             exception.printStackTrace();
-            Toast.makeText(this,"请完善信息！",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "请完善信息！", Toast.LENGTH_SHORT).show();
         }
     }
 
--- app/src/main/java/com/smapley/neibu/http/params/UpdateGhzParams.java
+++ app/src/main/java/com/smapley/neibu/http/params/UpdateGhzParams.java
@@ -11,12 +11,29 @@ import org.xutils.http.RequestParams;
  */
 public class UpdateGhzParams extends RequestParams {
 
-    public UpdateGhzParams(String user1, String erdj, String sandj, String sidj,
+    public UpdateGhzParams(String user1, String erdj1,
+                           String erdj2,
+                           String erdj3,
+                           String erdj4,
+                           String erdj5,
+                           String erdj6,
+                           String sandj1,
+                           String sandj2,
+                           String sandj3,
+                           String sandj4,String sidj,
                            String erxj, String sanxj, String sixj) {
         super(MyData.URL_updateGhz);
         addBodyParameter("user1", user1);
-        addBodyParameter("erdj", erdj);
-        addBodyParameter("sandj", sandj);
+        addBodyParameter("erdj1", erdj1);
+        addBodyParameter("erdj2", erdj2);
+        addBodyParameter("erdj3", erdj3);
+        addBodyParameter("erdj4", erdj4);
+        addBodyParameter("erdj5", erdj5);
+        addBodyParameter("erdj6", erdj6);
+        addBodyParameter("sandj1", sandj1);
+        addBodyParameter("sandj2", sandj2);
+        addBodyParameter("sandj3", sandj3);
+        addBodyParameter("sandj4", sandj4);
         addBodyParameter("sidj", sidj);
         addBodyParameter("erxj", erxj);
         addBodyParameter("sanxj", sanxj);
